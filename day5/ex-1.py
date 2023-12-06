02c97b1a1156c744679e27b96c2d70582fd98bbf
Syan Delbart <syan@syandelbart.com>
2023-12-06 01:18:48 +0100

    Modified ex1 of day5 to work with big numbers

--- day5/ex-1.py
+++ day5/ex-1.py
@@ -6,30 +6,23 @@ data_mappings = {}
 with open("./day5/input", "r") as file:
     lines = [line.strip() for line in file]
 
-
 seeds_list = lines[0][lines[0].find(":") + 2 :].split(" ")
-
 
 current_map = ""
 for line in lines[1:]:
     if line == "":
         continue
     parts = re.findall(r"\w+", line)
-    print(parts)
     if re.match(r"[a-z]+", parts[0]):
         current_map = parts[0] + "-" + parts[2]
-        data_mappings[current_map] = {}
+        data_mappings[current_map] = []
+
     else:
         parts = [int(x) for x in parts]
 
-        for [index, x] in enumerate(range(parts[1], parts[1] + parts[2])):
-            data_mappings[current_map][str(x)] = str(parts[0] + index)
-
-
-# for key in data_mappings:
-#     print(key)
-#     for x in sorted(data_mappings[key], key=lambda x: data_mappings[key][x]):
-#         print(x, data_mappings[key][x])
+        data_mappings[current_map].append(
+            {"destination": parts[0], "source": parts[1], "range": parts[2]}
+        )
 
 
 procedure = [
@@ -42,18 +35,23 @@ procedure = [
     "humidity",
     "location",
 ]
-seed_locations = {}
 
+seed_locations = {}
 for seed in seeds_list:
-    seed_locations[seed] = {"seed": seed}
+    seed_locations[seed] = {"seed": int(seed)}
     for x in range(0, len(procedure) - 1):
         map_key = procedure[x] + "-" + procedure[x + 1]
-
-        if seed_locations[seed][procedure[x]] not in data_mappings[map_key]:
-            current_value = seed_locations[seed][procedure[x]]
-        else:
-            current_value = data_mappings[map_key][seed_locations[seed][procedure[x]]]
-
+        current_value = seed_locations[seed][procedure[x]]
         seed_locations[seed][procedure[x + 1]] = current_value
+        for mapping in data_mappings[map_key]:
+            if (
+                mapping["source"]
+                <= current_value
+                < mapping["source"] + mapping["range"]
+            ):
+                seed_locations[seed][procedure[x + 1]] = (
+                    mapping["destination"] + current_value - mapping["source"]
+                )
+                break
 
 print("Lowest location:", min(map(lambda x: x["location"], seed_locations.values())))
